9f34ad0cd7409c7bcd7e3cc2a29eb6887af198ee
T.S. Foster <hi@tsf.io>
2019-03-13 11:03:55 +0000

    Add helper functions for Token

--- src/Token.elm
+++ src/Token.elm
@@ -1,4 +1,4 @@
-module Token exposing (Token, decoder, encode, named)
+module Token exposing (Token, decoder, default, encode, map, name, named)
 
 import Json.Decode as Decode exposing (Decoder)
 import Json.Encode as Encode exposing (Value)
@@ -17,15 +17,34 @@ named =
     Token
 
 
+map : (a -> b) -> Token a -> Token b
+map fn (Token theName theDefault) =
+    Token theName (fn theDefault)
+
+
+
+-- INSPECTING
+
+
+name : Token a -> String
+name (Token theName _) =
+    theName
+
+
+default : Token a -> a
+default (Token _ theDefault) =
+    theDefault
+
+
 
 -- ENCODE
 
 
 encode : (a -> Value) -> Token a -> Value
-encode encodeDefault (Token name default) =
+encode encodeDefault (Token theName theDefault) =
     Encode.object
-        [ ( "name", Encode.string name )
-        , ( "default", encodeDefault default )
+        [ ( "name", Encode.string theName )
+        , ( "default", encodeDefault theDefault )
         ]
 
 
